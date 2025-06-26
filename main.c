8c2b829086a80354aa41dc0d7a5be8c43afa69b0
Artem Tyanutov <artem.lab@gmail.com>
2025-06-26 21:30:12 +0500

    pico board led blink

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
-
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
 
 
 int main(void)
 {
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, true);
+    
     for(;;){
-        //
+        gpio_put(PICO_DEFAULT_LED_PIN, false);
+        sleep_ms(500);
+        gpio_put(PICO_DEFAULT_LED_PIN, true);
+        sleep_ms(500);
     }
 
     return 0;
